b71cf718129d3614b744c58551f492648902c524
Elena Sedunova <ellen.sedunova@gmail.com>
2024-10-25 18:51:14 +0300

    added some views

--- Cleaner/Presentation/Contacts/NoNumberContacts/NoNumberContactsViewController.swift
+++ Cleaner/Presentation/Contacts/NoNumberContacts/NoNumberContactsViewController.swift
@@ -6,7 +6,42 @@
 //
 
 import UIKit
+import Contacts
 
 final class NoNumberContactsViewController: UIViewController {
+    @IBOutlet weak var arrowBackButton: UIView!
+    @IBOutlet weak var unresolvedContactsCount: Regular13LabelStyle!
     
+    private var contacts: [CNContact] = [] {
+        didSet {
+            unresolvedContactsCount.bind(text: "\(contacts.count) contact\(contacts.count == 1 ? "" : "s")")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        addGestureRecognizers()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        reloadData()
+    }
+    
+    private func reloadData() {
+        ContactManager.loadIncompletedByName { contacts in
+            self.contacts = contacts
+        }
+    }
+}
+
+extension NoNumberContactsViewController: ViewControllerProtocol {
+    func setupUI() {}
+    
+    func addGestureRecognizers() {
+        arrowBackButton.addTapGestureRecognizer { [weak self] in
+            self?.navigationController?.popViewController(animated: true)
+        }
+    }
 }
