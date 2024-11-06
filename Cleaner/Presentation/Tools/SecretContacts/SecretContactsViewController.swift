c9d5a218816028b678c00ab211bded3f766fbb93
Elena Sedunova <ellen.sedunova@gmail.com>
2024-11-06 18:44:47 +0300

    first version of ui

--- Cleaner/Presentation/Tools/SecretContacts/SecretContactsViewController.swift
+++ Cleaner/Presentation/Tools/SecretContacts/SecretContactsViewController.swift
@@ -6,7 +6,45 @@
 //
 
 import UIKit
+import Contacts
 
 final class SecretContactsViewController: UIViewController {
+    @IBOutlet weak var arrowBackView: UIView!
+    @IBOutlet weak var itemsCountLabel: Regular13LabelStyle!
+    @IBOutlet weak var lockedStatusIcon: UIImageView!
     
+    private lazy var contacts: [CNContact] = [] {
+        didSet {
+            itemsCountLabel.bind(text: "\(contacts.count) contact\(contacts.count == 1 ? "" : "s")")
+//            itemsCollectionView.reloadData()
+//            if items.isEmpty {
+//                setupEmptyState()
+//            } else {
+//                hideEmptyState()
+//            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addGestureRecognizers()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        setupUI()
+//        reloadData()
+    }
+}
+
+extension SecretContactsViewController: ViewControllerProtocol {
+    func setupUI() {
+//        lockedStatusIcon.image = isPasscodeCreated ? .locked :  .unlocked
+    }
+    
+    func addGestureRecognizers() {
+        arrowBackView.addTapGestureRecognizer { [weak self] in
+            self?.navigationController?.popViewController(animated: true)
+        }
+    }
 }
