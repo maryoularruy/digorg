bba65b1de10ead9f23643fb9f9b4b2dbff343606
Elena Sedunova <ellen.sedunova@gmail.com>
2024-11-11 14:27:26 +0300

    first version of a page controller

--- Cleaner/Presentation/Tools/Battery/Components/Instructions/OptimizeBatteryCharging/OptimizeBatteryChargingView.swift
+++ Cleaner/Presentation/Tools/Battery/Components/Instructions/OptimizeBatteryCharging/OptimizeBatteryChargingView.swift
@@ -10,6 +10,18 @@ import UIKit
 final class OptimizeBatteryChargingView: UIView {
     lazy var arrowBack: UIView = arrowBackButton
     
+    private lazy var label: Semibold15LabelStyle = {
+        let label = Semibold15LabelStyle()
+        label.bind(text: "Optimize Battery Charging")
+        return label
+    }()
+    
+    lazy var pageController: UIPageViewController = {
+        let vc = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: .none)
+        vc.view.backgroundColor = .acidGreen
+        return vc
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -27,13 +39,24 @@ final class OptimizeBatteryChargingView: UIView {
     }
     
     private func initConstraints() {
-        addSubviews([arrowBackButton])
+        addSubviews([arrowBackButton,
+                     label,
+                     pageController.view
+                    ])
         
         NSLayoutConstraint.activate([
             arrowBackButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 4),
             arrowBackButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             arrowBackButton.heightAnchor.constraint(equalToConstant: arrowBackButton.frame.height),
-            arrowBackButton.widthAnchor.constraint(equalToConstant: arrowBackButton.frame.width)
+            arrowBackButton.widthAnchor.constraint(equalToConstant: arrowBackButton.frame.width),
+            
+            label.topAnchor.constraint(equalTo: arrowBackButton.bottomAnchor, constant: 16),
+            label.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 0),
+
+            pageController.view.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 16),
+            pageController.view.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            pageController.view.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
+            pageController.view.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -110)
         ])
     }
 }
--- Cleaner/Presentation/Tools/Battery/Components/Instructions/OptimizeBatteryCharging/OptimizeBatteryChargingViewController.swift
+++ Cleaner/Presentation/Tools/Battery/Components/Instructions/OptimizeBatteryCharging/OptimizeBatteryChargingViewController.swift
@@ -34,6 +34,9 @@ enum Pages: CaseIterable {
 
 final class OptimizeBatteryChargingViewController: UIViewController {
     private let rootView = OptimizeBatteryChargingView()
+//    private var pageController: UIPageViewController?
+    private var pages: [Pages] = Pages.allCases
+    private var currentIndex: Int = 0
     
     override func loadView() {
         super.loadView()
@@ -43,17 +46,48 @@ final class OptimizeBatteryChargingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         addGestureRecognizers()
+        setupUI()
+    }
+    
+    private func setupPageController() {
+        rootView.pageController.dataSource = self
+        rootView.pageController.delegate = self
+        addChild(rootView.pageController)
+        
+//        let initialVC = PageVC(with: pages[0])
+        
+//        self.pageController?.setViewControllers([initialVC], direction: .forward, animated: true, completion: nil)
+        
+        rootView.pageController.didMove(toParent: self)
     }
 }
 
 extension OptimizeBatteryChargingViewController: ViewControllerProtocol {
     func setupUI() {
-        
+        setupPageController()
     }
     
     func addGestureRecognizers() {
         rootView.arrowBack.addTapGestureRecognizer { [weak self] in
             self?.dismiss(animated: true)
         }
+    }
+}
+
+extension OptimizeBatteryChargingViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        UIViewController()
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        UIViewController()
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        pages.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        currentIndex
     }
 }
