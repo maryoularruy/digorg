54a34233d2d1760d90488daf73b4ca566fe41ab5
Elena Sedunova <ellen.sedunova@gmail.com>
2024-11-13 13:55:34 +0300

    LowPowerModeVC: added scrollView

--- Cleaner/Presentation/Tools/Battery/Components/Instructions/LowPowerMode/LowPowerModeView.swift
+++ Cleaner/Presentation/Tools/Battery/Components/Instructions/LowPowerMode/LowPowerModeView.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 final class LowPowerModeView: UIView {
+    private lazy var scroll: UIScrollView = scrollView
+    private lazy var contentView: UIView = UIView()
     lazy var arrowBack: UIView = arrowBackButton
     
     private lazy var label: Semibold15LabelStyle = {
@@ -33,16 +35,30 @@ final class LowPowerModeView: UIView {
     }
     
     private func initConstraints() {
-        addSubviews([arrowBack, label])
+        addSubviews([scroll])
+        scroll.addSubviews([contentView])
+        contentView.addSubviews([arrowBack, label])
         
         NSLayoutConstraint.activate([
-            arrowBack.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 4),
-            arrowBack.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            scroll.topAnchor.constraint(equalTo: topAnchor),
+            scroll.leadingAnchor.constraint(equalTo: leadingAnchor),
+            scroll.trailingAnchor.constraint(equalTo: trailingAnchor),
+            scroll.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scroll.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scroll.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scroll.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scroll.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scroll.widthAnchor, constant: -32),
+            contentView.heightAnchor.constraint(equalToConstant: 1000),
+            
+            arrowBack.topAnchor.constraint(equalTo: contentView.topAnchor),
+            arrowBack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             arrowBack.heightAnchor.constraint(equalToConstant: arrowBackButton.frame.height),
             arrowBack.widthAnchor.constraint(equalToConstant: arrowBackButton.frame.width),
             
             label.topAnchor.constraint(equalTo: arrowBack.bottomAnchor, constant: 16),
-            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16)
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor)
         ])
     }
 }
---
+++ Cleaner/Presentation/Tools/Components/ReusableProgrammatically/ScrollView.swift
@@ -0,0 +1,15 @@
+//
+//  ScrollView.swift
+//  Cleaner
+//
+//  Created by Elena Sedunova on 13.11.2024.
+//
+
+import UIKit
+
+var scrollView: UIScrollView = {
+    let scrollView = UIScrollView()
+    scrollView.contentInset = UIEdgeInsets(top: 4, left: 16, bottom: 36, right: 16)
+    scrollView.backgroundColor = .clear
+    return scrollView
+}()
