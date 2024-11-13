27239a40ee1a46b0b58b58e6cdcec88e31f42783
Elena Sedunova <ellen.sedunova@gmail.com>
2024-11-13 14:30:08 +0300

    added new assets

--- Cleaner/Resources/Assets/UIImage.swift
+++ Cleaner/Resources/Assets/UIImage.swift
@@ -30,6 +30,7 @@ extension UIImage {
     static var widgets = UIImage(resource: .widgets)
     static var battery = UIImage(resource: .battery)
     
+    //MARK: -SecretAlbumTool
     static var mediaIcon = UIImage(resource: .mediaIcon)
     static var addButton = UIImage(resource: .addButton)
     static var unlocked = UIImage(resource: .unlocked)
@@ -40,6 +41,7 @@ extension UIImage {
     static var importMedia = UIImage(resource: .importMedia)
     static var takeMedia = UIImage(resource: .takeMedia)
     
+    //MARK: -BatterySavingTool
     static var optimizeBatteryCharging = UIImage(resource: .optimizeBatteryCharging)
     static var lowPowerMode = UIImage(resource: .lowPowerMode)
     static var managingConnections = UIImage(resource: .managingConnections)
@@ -50,10 +52,37 @@ extension UIImage {
     static var wifiRefresh = UIImage(resource: .wiFiResresh)
     static var limitNotifications = UIImage(resource: .limitNotifications)
     static var overheating = UIImage(resource: .overheating)
+    
     static var batteryInstructions1 = UIImage(resource: .batteryInstructions1)
     static var batteryInstructions2 = UIImage(resource: .batteryInstructions2)
     static var batteryInstructions3 = UIImage(resource: .batteryInstructions3)
     static var batteryInstructions4 = UIImage(resource: .batteryInstructions4)
+    
+    static var lowPowerMode1 = UIImage(resource: .lowPowerMode1)
+    static var lowPowerMode2 = UIImage(resource: .lowPowerMode2)
+    
+    static var locationServices1 = UIImage(resource: .locationServices1)
+    static var locationServices2 = UIImage(resource: .locationServices2)
+    static var locationServices3 = UIImage(resource: .locationServices3)
+    
+    static var backgroundRefresh1 = UIImage(resource: .backgroundRefresh1)
+    
+    static var brightness1 = UIImage(resource: .brightness1)
+    static var brightness2 = UIImage(resource: .brightness2)
+    static var brightness3 = UIImage(resource: .brightness3)
+    
+    static var limitNotifications1 = UIImage(resource: .limitNotifications1)
+    static var limitNotifications2 = UIImage(resource: .limitNotifications2)
+    
+    static var wifiRefresh1 = UIImage(resource: .wiFiResfesh1)
+    
+    static var overheating1 = UIImage(resource: .overheating1)
+    
+    static var batteryUsage1 = UIImage(resource: .batteryUsage1)
+    static var batteryUsage2 = UIImage(resource: .batteryUsage2)
+    
+    static var managingConnections1 = UIImage(resource: .managingConnections1)
+    static var managingConnections2 = UIImage(resource: .managingConnections2)
     
     //MARK: -Navigation
     static var arrowBackIcon = UIImage(resource: .arrowBackIcon)
