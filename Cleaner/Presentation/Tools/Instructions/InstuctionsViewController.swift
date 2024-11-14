5b83a6dac9ed4e9d575c396510f0c37bcb4c5183
Elena Sedunova <ellen.sedunova@gmail.com>
2024-11-14 20:14:00 +0300

    added necessary files

---
+++ Cleaner/Presentation/Tools/Instructions/InstuctionsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  InstuctionsViewController.swift
+//  Cleaner
+//
+//  Created by Elena Sedunova on 14.11.2024.
+//
+
+import UIKit
+
+final class InstuctionsViewController: UIViewController {
+    
+}
