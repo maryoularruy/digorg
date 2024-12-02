29547dbaf17b870ac2cf1d44547f818a9fb62e6b
Elena Sedunova <ellen.sedunova@gmail.com>
2024-12-02 22:53:35 +0300

    setuped VideoTotalView

--- Cleaner/Presentation/PhotoVideo/Total/VideoTotalViewController.swift
+++ Cleaner/Presentation/PhotoVideo/Total/VideoTotalViewController.swift
@@ -8,5 +8,41 @@
 import UIKit
 
 final class VideoTotalViewController: UIViewController {
+    private lazy var rootView = VideoTotalView()
+    private lazy var photoVideoManager = PhotoVideoManager.shared
     
+    override func loadView() {
+        super.loadView()
+        view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addGestureRecognizers()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupUI()
+    }
+}
+
+extension VideoTotalViewController: ViewControllerProtocol {
+    func setupUI() {
+        
+    }
+    
+    func addGestureRecognizers() {
+        rootView.arrowBack.addTapGestureRecognizer { [weak self] in
+            self?.navigationController?.popViewController(animated: true)
+        }
+        
+        let swipeRightGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeRight))
+        swipeRightGesture.direction = .right
+        view.addGestureRecognizer(swipeRightGesture)
+    }
+    
+    @objc private func handleSwipeRight() {
+        navigationController?.popViewController(animated: true)
+    }
 }
