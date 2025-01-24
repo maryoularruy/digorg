2b97f5a0c1a39d1f06a805a52eafb08f25ef2d0c
Elena Sedunova <ellen.sedunova@gmail.com>
2025-01-24 20:33:15 +0300

    rewrote SpeedTestProgressBar

--- Cleaner/Presentation/NetworkSpeedTest/Components/SpeedTestProgressBar.swift
+++ Cleaner/Presentation/NetworkSpeedTest/Components/SpeedTestProgressBar.swift
@@ -10,18 +10,20 @@ import UIKit
 final class SpeedTestProgressBar: UIView {
     private lazy var backgroundLayer = CAShapeLayer()
     private lazy var progressLayer = CAShapeLayer()
+    
     private lazy var startPoint = CGFloat(7 * Double.pi / 8)
     private lazy var endPoint = CGFloat(2 * Double.pi + Double.pi / 8)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupBackgroundLayer()
-        setupProgressLayer()
+        setupLayer(&backgroundLayer, isBackgroundLayer: true)
+        setupLayer(&progressLayer, isBackgroundLayer: false)
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        setupProgressLayer()
+        setupLayer(&backgroundLayer, isBackgroundLayer: true)
+        setupLayer(&progressLayer, isBackgroundLayer: false)
     }
     
     func progressAnimation(_ endValue: Double) {
@@ -33,27 +35,17 @@ final class SpeedTestProgressBar: UIView {
         progressLayer.add(circularProgressAnimation, forKey: "progressAnim")
     }
     
-    private func setupBackgroundLayer() {
-        backgroundLayer.path = createCircularPath().cgPath
-        backgroundLayer.fillColor = UIColor.clear.cgColor
-        backgroundLayer.lineCap = .round
-        backgroundLayer.lineWidth = 28.0
-        backgroundLayer.strokeEnd = 1.0
-        backgroundLayer.strokeColor = UIColor.lightGrey.cgColor
-        layer.addSublayer(backgroundLayer)
-    }
-    
-    private func setupProgressLayer() {
-        progressLayer.path = createCircularPath().cgPath
-        progressLayer.fillColor = UIColor.clear.cgColor
-        progressLayer.lineCap = .round
-        progressLayer.lineWidth = 28.0
-        progressLayer.strokeEnd = 0
-        progressLayer.strokeColor = UIColor.blue.cgColor
-        layer.addSublayer(progressLayer)
-    }
-    
     private func createCircularPath() -> UIBezierPath {
         UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: 110, startAngle: startPoint, endAngle: endPoint, clockwise: true)
+    }
+    
+    private func setupLayer(_ layer: inout CAShapeLayer, isBackgroundLayer: Bool) {
+        layer.path = createCircularPath().cgPath
+        layer.fillColor = UIColor.clear.cgColor
+        layer.lineCap = .round
+        layer.lineWidth = 28.0
+        layer.strokeEnd = isBackgroundLayer ? 1.0 : 0
+        layer.strokeColor = isBackgroundLayer ? UIColor.lightGrey.cgColor : UIColor.blue.cgColor
+        self.layer.addSublayer(layer)
     }
 }
