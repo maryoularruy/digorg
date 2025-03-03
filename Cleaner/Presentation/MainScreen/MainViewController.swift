70edca6155783d08c49ed223246d78f3ceb78715
Elena Sedunova <ellen.sedunova@gmail.com>
2025-03-03 19:09:47 +0300

    fixed error

--- Cleaner/Presentation/MainScreen/MainViewController.swift
+++ Cleaner/Presentation/MainScreen/MainViewController.swift
@@ -121,32 +121,37 @@ final class MainViewController: UIViewController {
     }
     
     private func updatePhotosCleanupOption() {
-        photosCleanup.infoButton.startSpinner()
-        photosCleanup.isUserInteractionEnabled = false
-        photoVideoManager.fetchSimilarPhotos(live: false) { assetsGroups, duplicatesCount, size in
-            DispatchQueue.main.async { [weak self] in
-                self?.photosCleanup.infoButton.endSpinner()
-                self?.photosCleanup.infoButton.bind(text: "\(duplicatesCount) file\(duplicatesCount == 1 ? "" : "s"), \(size.roundAndToString())")
-                self?.photosCleanup.isUserInteractionEnabled = true
-            }
+        DispatchQueue.main.async { [weak self] in
+            self?.photosCleanup.infoButton.startSpinner()
+            self?.photosCleanup.isUserInteractionEnabled = false
+        }
+        
+        photoVideoManager.fetchSimilarPhotos(live: false) { [weak self] assetsGroups, duplicatesCount, size in
+            self?.photosCleanup.infoButton.endSpinner()
+            self?.photosCleanup.infoButton.bind(text: "\(duplicatesCount) file\(duplicatesCount == 1 ? "" : "s"), \(size.roundAndToString())")
+            self?.photosCleanup.isUserInteractionEnabled = true
         }
     }
     
     private func updateVideosCleanupOption() {
-        videosCleanup.infoButton.startSpinner()
-        videosCleanup.isUserInteractionEnabled = false
-        photoVideoManager.fetchSimilarVideos { _, duplicatesCount, size in
-            DispatchQueue.main.async { [weak self] in
-                self?.videosCleanup.infoButton.endSpinner()
-                self?.videosCleanup.infoButton.bind(text: "\(duplicatesCount) file\(duplicatesCount == 1 ? "" : "s"), \(size.roundAndToString())")
-                self?.videosCleanup.isUserInteractionEnabled = true
-            }
+        DispatchQueue.main.async { [weak self] in
+            self?.videosCleanup.infoButton.startSpinner()
+            self?.videosCleanup.isUserInteractionEnabled = false
+        }
+        
+        photoVideoManager.fetchSimilarVideos { [weak self] _, duplicatesCount, size in
+            self?.videosCleanup.infoButton.endSpinner()
+            self?.videosCleanup.infoButton.bind(text: "\(duplicatesCount) file\(duplicatesCount == 1 ? "" : "s"), \(size.roundAndToString())")
+            self?.videosCleanup.isUserInteractionEnabled = true
         }
     }
     
     private func updateContactsCleanupOption() {
-        contactsCleanup.infoButton.startSpinner()
-        contactsCleanup.isUserInteractionEnabled = false
+        DispatchQueue.main.async { [weak self] in
+            self?.contactsCleanup.infoButton.startSpinner()
+            self?.contactsCleanup.isUserInteractionEnabled = false
+        }
+        
         contactManager.countUnresolvedContacts { _, _, _, _, summaryCount in
             DispatchQueue.main.async { [weak self] in
                 self?.contactsCleanup.infoButton.endSpinner()
@@ -157,8 +162,11 @@ final class MainViewController: UIViewController {
     }
     
     private func updateCalendarCleanupOption() {
-        calendarCleanup.infoButton.startSpinner()
-        calendarCleanup.isUserInteractionEnabled = false
+        DispatchQueue.main.async { [weak self] in
+            self?.calendarCleanup.infoButton.startSpinner()
+            self?.calendarCleanup.isUserInteractionEnabled = false
+        }
+
         calendarManager.fetchEvents { events in
             DispatchQueue.main.async { [weak self] in
                 self?.calendarCleanup.infoButton.endSpinner()
