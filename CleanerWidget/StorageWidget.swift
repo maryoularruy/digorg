3648d96cab315df41791f52f100a5ad3e1819075
Elena Sedunova <ellen.sedunova@gmail.com>
2025-04-03 14:26:24 +0300

    created storage widget

--- CleanerWidget/StorageWidget.swift
+++ CleanerWidget/StorageWidget.swift
@@ -10,37 +10,130 @@ import SwiftUI
 
 struct StorageProvider: TimelineProvider {
     func placeholder(in context: Context) -> StorageWidgetEntry {
-        StorageWidgetEntry(date: Date())
+        StorageWidgetEntry(date: .now, totalSize: 128, busySize: 100, busySizeInPercent: 78)
     }
 
     func getSnapshot(in context: Context, completion: @escaping (StorageWidgetEntry) -> ()) {
-        completion(StorageWidgetEntry(date: Date()))
+        completion(getCurrentStorageStateEntry())
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [StorageWidgetEntry] = []
         
-        let entry = StorageWidgetEntry(date: Date())
+        let entry = getCurrentStorageStateEntry()
         let nextUpdationDate = Calendar.current.date(byAdding: .minute, value: 30, to: entry.date)!
         entries.append(entry)
         completion(Timeline(entries: entries, policy: .after(nextUpdationDate)))
+    }
+    
+    private func getCurrentStorageStateEntry() -> StorageWidgetEntry {
+        let totalSize = FileManager.default.getTotalStorageSizeInGigabytes() ?? 0
+        let freeSize = FileManager.default.getFreeStorageSizeInGigabytes() ?? 0
+        let busySize = totalSize - freeSize
+        let busySizeInPercent = (busySize * 100) / totalSize
+        
+        let color: Color
+        let isWhiteBackground: Bool
+        
+        if let colorHex = UserDefaultsService.shared.storageWidgetHexBackground,
+           let uiColor = UIColor(hex: colorHex) {
+            color = Color(uiColor)
+            isWhiteBackground = colorHex == "F9FAFC"
+        } else {
+            color = Color(.blue)
+            isWhiteBackground = false
+        }
+        
+        return StorageWidgetEntry(date: .now,
+                                  totalSize: totalSize,
+                                  busySize: busySize,
+                                  busySizeInPercent: busySizeInPercent,
+                                  backgroundColor: color,
+                                  isWhiteBackground: isWhiteBackground)
     }
 }
 
 
 struct StorageWidgetEntry: TimelineEntry {
     let date: Date
+    let totalSize: Int
+    let busySize: Int
+    let busySizeInPercent: Int
     var backgroundColor: Color = .blue
     var isWhiteBackground: Bool = false
 }
 
 struct StorageWidgetEntryView : View {
+    @Environment(\.widgetFamily) var widgetFamily
     
     var entry: StorageProvider.Entry
     
     var body: some View {
-        HStack {
+        switch widgetFamily {
+        case .systemSmall: smallView()
+        case .systemMedium: mediumView()
+        default: smallView()
+        }
+    }
+    
+    func smallView() -> some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                    Text("Storage")
+                        .font(.regular13)
+                        .foregroundStyle(entry.isWhiteBackground ? Color(.darkGrey) : Color(.lightGrey))
+                    
+                    Text("\(entry.busySizeInPercent) %")
+                        .font(.semibold24)
+                        .foregroundStyle(entry.isWhiteBackground ? Color(.black) : Color(.paleGrey))
+                }
+                Spacer()
+                Image(entry.isWhiteBackground ? .storageWidgetSmallBlueIcon : .storageWidgetSmallWhiteIcon)
+                    .frame(width: 40, height: 40, alignment: .trailing)
+            }
             
+            Spacer()
+            
+            Text("Storage Used")
+                .font(.regular13)
+                .foregroundStyle(entry.isWhiteBackground ? Color(.darkGrey) : Color(.lightGrey))
+            
+            Text("\(entry.busySize)GB / \(entry.totalSize)GB")
+                .font(.medium13)
+                .lineLimit(2)
+                .foregroundStyle(entry.isWhiteBackground ? Color(.black) : Color(.paleGrey))
+        }
+    }
+    
+    func mediumView() -> some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                    Text("Storage")
+                        .font(.regular13)
+                        .foregroundStyle(entry.isWhiteBackground ? Color(.darkGrey) : Color(.lightGrey))
+                    
+                    Text("\(entry.busySizeInPercent) %")
+                        .font(.semibold24)
+                        .foregroundStyle(entry.isWhiteBackground ? Color(.black) : Color(.paleGrey))
+                    
+                    Text("Storage Used")
+                        .padding(.top, 8)
+                        .font(.regular13)
+                        .foregroundStyle(entry.isWhiteBackground ? Color(.darkGrey) : Color(.lightGrey))
+                    
+                    Text("\(entry.busySize)GB / \(entry.totalSize)GB")
+                        .font(.medium13)
+                        .foregroundStyle(entry.isWhiteBackground ? Color(.black) : Color(.paleGrey))
+                }
+                
+                Spacer()
+                
+                Image(entry.isWhiteBackground ? .storageWidgetMediumBlueIcon : .storageWidgetMediumWhiteIcon)
+                    .frame(width: 40, height: 40, alignment: .center)
+                    .padding(EdgeInsets(top: 70, leading: 0, bottom: 0, trailing: 9))
+            }
         }
     }
 }
